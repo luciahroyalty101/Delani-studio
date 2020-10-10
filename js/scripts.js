ae73baa7c79f83a765c00cb6ffa954a218ca935a
luciahroyalty101 <luciahkei101@gmail.com>
2020-10-10 21:52:50 +0300

    modify scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,84 +1,90 @@
-$().ready(function(){
-$(".design").click(function(){
-$(".par").toggle("slow");
+$().ready(function () {
+  $(".design").click(function () {
+    $(".par").toggle("slow");
+  });
 });
+$().ready(function () {
+  $("#lucy").click(function () {
+    $("#para").toggle("slow");
+  });
 });
-$().ready(function(){
-$("#lucy").click(function(){
-$("#para").toggle("slow");
+$().ready(function () {
+  $("#jim").click(function () {
+    $("#karimi").toggle("slow");
+  });
 });
+$("button").click(function () {
+  var form1 = document.getElementById("form1").value;
+  var form2 = document.getElementById("form2").value;
+  var comment = document.getElementById("comment").value;
+  if (form1 === "") {
+    alert("please enter appropriate name")
+  } else
+  if (form2 === "") {
+    alert("please enter appropriate email")
+  } else
+  if (comment === "") {
+    alert("please enter a comment")
+  } else {
+    alert(" Dear " + " esteemed " + form1 + " we have received your message succecifully. " + " Feel free to reach out unto us anytime");
+  }
 });
-$().ready(function(){
-$("#jim").click(function(){
-$("#karimi").toggle("slow");
- });
-});
-// $().ready(function(){
-//  $("#wangeci").hover(function(){
-//     $("#frank").hover("red");
-//  });
-// });
-$("button").click(function(wanjii){
-  var customer = document.getElementById("june").value;
-  alert(" Dear " +" esteemed " + customer + " we have received your message succecifully. " +" Feel free to reach out unto us anytime");
-  wanjii.preventDefault();
-});
-$().ready(function(){
-$("#img1").mouseover(function (){
-   $("#frank").show();
-  }).mouseout(function(){
+$().ready(function () {
+  $("#img1").mouseover(function () {
+    $("#frank").show();
+  }).mouseout(function () {
     $("#frank").hide();
   });
 });
-$().ready(function(){
-  $(".img2").mouseover(function (){
-     $("#pro2").show();
-    }).mouseout(function(){
-      $("#pro2").hide();
-    });
+$().ready(function () {
+  $(".img2").mouseover(function () {
+    $("#pro2").show();
+  }).mouseout(function () {
+    $("#pro2").hide();
   });
-  $().ready(function(){
-    $(".img3").mouseover(function (){
-       $("#pro3").show();
-      }).mouseout(function(){
-        $("#pro3").hide();
-      });
-    });
-    $().ready(function(){
-      $(".img4").mouseover(function (){
-         $("#pro4").show();
-        }).mouseout(function(){
-          $("#pro4").hide();
-        });
-      });
-      $().ready(function(){
-        $(".img5").mouseover(function (){
-           $("#pro5").show();
-          }).mouseout(function(){
-            $("#pro5").hide();
-          });
-        });
-        $().ready(function(){
-          $(".img6").mouseover(function (){
-             $("#pro6").show();
-            }).mouseout(function(){
-              $("#pro6").hide();
-            });
-          });
-          $().ready(function(){
-            $(".img7").mouseover(function (){
-               $("#pro7").show();
-              }).mouseout(function(){
-                $("#pro7").hide();
-              });
-            });
-            $().ready(function(){
-              $(".img8").mouseover(function (){
-                 $("#pro8").show();
-                }).mouseout(function(){
-                  $("#pro8").hide();
-                });
-              });
+});
+$().ready(function () {
+  $(".img3").mouseover(function () {
+    $("#pro3").show();
+  }).mouseout(function () {
+    $("#pro3").hide();
+  });
+});
+$().ready(function () {
+  $(".img4").mouseover(function () {
+    $("#pro4").show();
+  }).mouseout(function () {
+    $("#pro4").hide();
+  });
+});
+$().ready(function () {
+  $(".img5").mouseover(function () {
+    $("#pro5").show();
+  }).mouseout(function () {
+    $("#pro5").hide();
+  });
+});
+$().ready(function () {
+  $(".img6").mouseover(function () {
+    $("#pro6").show();
+  }).mouseout(function () {
+    $("#pro6").hide();
+  });
+});
+$().ready(function () {
+  $(".img7").mouseover(function () {
+    $("#pro7").show();
+  }).mouseout(function () {
+    $("#pro7").hide();
+  });
+});
+$().ready(function () {
+  $(".img8").mouseover(function () {
+    $("#pro8").show();
+  }).mouseout(function () {
+    $("#pro8").hide();
+  });
+});
 
 
 
@@ -129,7 +135,7 @@ $().ready(function(){
 //     $( this ).find( "span" ).last().remove();
 //   }
 // );
- 
+
 // $( "#frank" ).hover(function() {
 //   $( this ).fadeOut( 100 );
 //   $( this ).fadeIn( 500 );
