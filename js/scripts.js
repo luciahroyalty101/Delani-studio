187a45b546cf19d37408aa6b98f00c86e4e910bb
luciahroyalty101 <luciahkei101@gmail.com>
2020-10-10 17:27:04 +0300

    modify scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,59 @@ $("button").click(function(wanjii){
   alert(" Dear " +" esteemed " + customer + " we have received your message succecifully. " +" Feel free to reach out unto us anytime");
   wanjii.preventDefault();
 });
+$().ready(function(){
+$("#img1").mouseover(function (){
+   $("#frank").show();
+  }).mouseout(function(){
+    $("#frank").hide();
+  });
+});
+$().ready(function(){
+  $(".img2").mouseover(function (){
+     $("#pro2").show();
+    }).mouseout(function(){
+      $("#pro2").hide();
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $( "#wangeci" )
+//   .odd()
+//     .hide()
+//   .end()
+//   .even()
+//     .hover(function() {
+//       $( this )
+//         .toggleClass( "active" )
+//         .next()
+//           .stop( true, true )
+//           .slideToggle();
+//     });
+// $( "#wangeci" ).hover(
+//   function() {
+//     $( this ).append( $( "<span> ***</span>" ) );
+//   }, function() {
+//     $( this ).find( "span" ).last().remove();
+//   }
+// );
+ 
+// $( "#frank" ).hover(function() {
+//   $( this ).fadeOut( 100 );
+//   $( this ).fadeIn( 500 );
+// });
